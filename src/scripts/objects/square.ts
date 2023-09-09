cfebf50cb966e557407092c92b3103b646e5a39e
pintupal1996 <supriyomukherjee111@gmail.com>
2023-09-10 00:19:18 +0530

    movement direction
    movement direction

--- src/scripts/objects/square.ts
+++ src/scripts/objects/square.ts
@@ -14,9 +14,64 @@ export default class Square extends Phaser.GameObjects.Rectangle{
         this.setFillStyle(0x0000ff, 1)
     }
 
-    public move(time, direction: {x: number, y: number}){
+    public move(time, direction: {x: number, y: number},headDirection: {x: number, y: number}){
         this.x += direction.x * CELL_SIZE
         this.y += direction.y * CELL_SIZE
+        console.log('dir :---- ',direction.x,direction.y)
+
+        if(direction.x == 0 && direction.y == 0)
+        { 
+
+        }
+        else{
+            console.log('non zero')
+
+            // if()s
+        }
+
+
+        // if(direction.x == 0 && direction.y == -1 ||
+        //   direction.x == 0 && direction.y == 1) {
+        //   headDirection.x = -1;
+        //   headDirection.y = 0; 
+
+        //   this.x += headDirection.x * CELL_SIZE
+        //   this.y += headDirection.y * CELL_SIZE
+        // }
+        // else if(direction.x == 0 && direction.y == -1 ||
+        //   direction.x == 0 && direction.y == 1){
+        //   headDirection.x = 1;
+        //   headDirection.y = 0; 
+
+        //   this.x += headDirection.x * CELL_SIZE
+        //   this.y += headDirection.y * CELL_SIZE
+        // } 
+        // else if(direction.x == -1 && direction.y == 0 ||
+        //   direction.x == 1 && direction.y == 0){
+        //   headDirection.x = 0; 
+        //   headDirection.y = -1; 
+
+        //   this.x += headDirection.x * CELL_SIZE
+        //   this.y += headDirection.y * CELL_SIZE
+        // } 
+        // else if(direction.x == -1 && direction.y == 0 ||
+        //   direction.x == 1 && direction.y == 0){
+        //   headDirection.x = 0;
+        //   headDirection.y = 1; 
+
+        //   this.x += headDirection.x * CELL_SIZE
+        //   this.y += headDirection.y * CELL_SIZE
+        // }
+        // else{
+        //     this.x += direction.x * CELL_SIZE
+        //     this.y += direction.y * CELL_SIZE
+        // }
+
+
+
+
+
+
 
         if(this.x > DEFAULT_WIDTH){
             this.x = 0
@@ -36,12 +91,48 @@ export default class Square extends Phaser.GameObjects.Rectangle{
 
         this.moveTime = time + this.tickDelay;
     }
-
+    // faceLeft(direction: {x: number, y: number}){
+    //     console.log('dir : ', this.direction)
+    //     if(this.direction.x == 0 && this.direction.y == -1 ||
+    //       this.direction.x == 0 && this.direction.y == 1){
+    //       this.headDirection.x = -1;
+    //       this.headDirection.y = 0;
+    //       // return this.headDirection;
+    //     }
+    //   }
+    //   faceRight(direction: {x: number, y: number}){
+    //     if(this.direction.x == 0 && this.direction.y == -1 ||
+    //       this.direction.x == 0 && this.direction.y == 1){
+    //       this.headDirection.x = 1;
+    //       this.headDirection.y = 0;
+    //       // return this.headDirection;
+    //     }
+    //   }
+    //   faceUp(direction: {x: number, y: number}){
+    //     if(this.direction.x == -1 && this.direction.y == 0 ||
+    //       this.direction.x == 1 && this.direction.y == 0){
+    //       this.headDirection.x = 0; 
+    //       this.headDirection.y = -1;
+    //       // return this.headDirection;
+    //     }
+    //   }
+    //   faceDown(direction: {x: number, y: number}){
+    //     if(this.direction.x == -1 && this.direction.y == 0 ||
+    //       this.direction.x == 1 && this.direction.y == 0){
+    //       this.headDirection.x = 0;
+    //       this.headDirection.y = 1;
+    //       this.direction = {x:this.headDirection.x,y:this.headDirection.y}
+    //       // return this.headDirection;
+    //     }
+    //   }
     public nextPos(direction: {x: number, y: number}){
         let x = this.x + direction.x * CELL_SIZE;
         let y = this.y + direction.y * CELL_SIZE; 
 
-        // console.log('x----->',x,y)
+        console.log('x----->',direction.x,direction.y)
+
+
+
 
         if(x > DEFAULT_WIDTH){
             x = 0
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -9,6 +9,7 @@ export default class MainScene extends Phaser.Scene {
   square: Square;
   food: Food;
   grid: Grid
+  headDirection : {x: number, y: number} = {x:0, y:0};
   direction: {x: number, y: number} = {x: 0, y: 0}
   cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
 
@@ -25,22 +26,55 @@ export default class MainScene extends Phaser.Scene {
     this.square = new Square(this, 0, 0);
   }
 
+  // faceLeft(direction: {x: number, y: number}){
+  //   console.log('dir : ', this.direction)
+  //   if(this.direction.x == 0 && this.direction.y == -1 ||
+  //     this.direction.x == 0 && this.direction.y == 1){
+  //     this.headDirection.x = -1;
+  //     this.headDirection.y = 0;
+  //     // return this.headDirection;
+  //   }
+  // }
+  // faceRight(direction: {x: number, y: number}){
+  //   if(this.direction.x == 0 && this.direction.y == -1 ||
+  //     this.direction.x == 0 && this.direction.y == 1){
+  //     this.headDirection.x = 1;
+  //     this.headDirection.y = 0;
+  //     // return this.headDirection;
+  //   }
+  // }
+  // faceUp(direction: {x: number, y: number}){
+  //   if(this.direction.x == -1 && this.direction.y == 0 ||
+  //     this.direction.x == 1 && this.direction.y == 0){
+  //     this.headDirection.x = 0; 
+  //     this.headDirection.y = -1;
+  //     // return this.headDirection;
+  //   }
+  // }
+  // faceDown(direction: {x: number, y: number}){
+  //   if(this.direction.x == -1 && this.direction.y == 0 ||
+  //     this.direction.x == 1 && this.direction.y == 0){
+  //     this.headDirection.x = 0;
+  //     this.headDirection.y = 1;
+  //     this.direction = {x:this.headDirection.x,y:this.headDirection.y}
+  //     // return this.headDirection;
+  //   }
+  // }
   update(time, delta) {
-    this.square.nextPos(this.direction)
-
+    // this.square.nextPos(this.direction) 
 
     if(this.cursorKeys.up.isDown){
-      this.direction = {x: 0, y: -1};
+      this.direction = {x: 0, y: -1};  
     }
     else if(this.cursorKeys.down.isDown){
-      this.direction = {x: 0, y: 1};
+      this.direction = {x: 0, y: 1}; 
     }
-    else if(this.cursorKeys.left.isDown){
+    else if(this.cursorKeys.left.isDown){ 
       this.direction = {x: -1, y: 0};
     }
-    else if(this.cursorKeys.right.isDown){
+    else if(this.cursorKeys.right.isDown){ 
       this.direction = {x: 1, y: 0};
-    }else{
+    } else{
       // this.direction = {x: 0, y: 0};
     }
     
@@ -54,14 +88,14 @@ export default class MainScene extends Phaser.Scene {
       let x = this.square.x - this.direction.x * CELL_SIZE;
       let y = this.square.y - this.direction.y * CELL_SIZE; 
 
-      this.food.setPosition(x,y);
-      this.food.moveFood(time,this.direction);
+      // this.food.setPosition(x,y);
+      // this.food.moveFood(time,this.direction);
     }
     
  
     
     if(time >= this.square.moveTime && this.direction){
-      this.square.move(time, this.direction)
+      this.square.move(time, this.direction, this.headDirection)
     }
   }
 }
