a1bb276f841ff7b2c6f9b30912d6b856cf9162a8
Priti Basak <75367921+pritibasak@users.noreply.github.com>
2023-09-14 19:15:49 +0530

    Snake game changes

--- src/scripts/objects/square.ts
+++ src/scripts/objects/square.ts
@@ -13,56 +13,37 @@ export default class Square extends Phaser.GameObjects.Rectangle{
         this.height = CELL_SIZE;
         this.setFillStyle(color, 1)
     }
+//A new parameter snake array added because after growing the snake body, whole body needs to move instead of one square
+//Also the snake body needs to be repositioned for each grow
+    public move(time, direction: {x: number, y: number},snake : Square[])
+    {
+         for (let i = snake.length - 1; i > 0; i--) { 
+            snake[i].x = snake[i - 1].x ;
+            snake[i].y = snake[i - 1].y ;
+        } 
 
-    public move(time, direction: {x: number, y: number}){
-        this.x += direction.x * CELL_SIZE
-        this.y += direction.y * CELL_SIZE
+        snake[0].x += direction.x * CELL_SIZE
+        snake[0].y += direction.y * CELL_SIZE 
 
-        if(this.x > DEFAULT_WIDTH){
-            this.x = 0
+        /*to wrap the snake around the screen, so when the snake
+        goes off any of the sides of the screen it re-appears*/
+       
+        if(snake[0].x > DEFAULT_WIDTH){
+            snake[0].x = 0
         }
 
-        if(this.y > DEFAULT_HEIGHT){
-            this.y = 0
+        if(snake[0].y > DEFAULT_HEIGHT){
+            snake[0].y = 0
         }
 
-        if(this.x < 0) {
-            this.x = DEFAULT_WIDTH-CELL_SIZE
+        if(snake[0].x < 0) {
+            snake[0].x = DEFAULT_WIDTH-CELL_SIZE
         }
 
-        if(this.y < 0){
-            this.y = DEFAULT_HEIGHT-CELL_SIZE
-        }
-
+        if(snake[0].y < 0){
+            snake[0].y = DEFAULT_HEIGHT-CELL_SIZE
+        } 
+ 
         this.moveTime = time + this.tickDelay;
-    }
-
-    public nextPos(direction: {x: number, y: number}){
-        let x = this.x + direction.x * CELL_SIZE;
-        let y = this.y + direction.y * CELL_SIZE; 
-
-        if(x > DEFAULT_WIDTH){
-            x = 0
-        }
-
-        if(y > DEFAULT_HEIGHT){
-            y = 0
-        }
-
-        if(x < 0) {
-            x = DEFAULT_WIDTH-CELL_SIZE
-        }
-
-        if(y < 0){
-            y = DEFAULT_HEIGHT-CELL_SIZE
-        }
-
-        return {x, y}
-
-    }
-
-    public moveTo(time: number, x: number,y: number){
-        this.x = x;
-        this.y = y;
     }
 }
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,17 +2,17 @@ import FpsText from '../objects/fpsText'
 import Square from '../objects/square'
 import Grid from '../objects/grid'
 import { CELL_SIZE, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../game'
-
 export default class MainScene extends Phaser.Scene {
   fpsText: FpsText
   square: Square
+  //head: Square
+  //tail: Square
   food: Square
   grid: Grid
   snakeArray: Square[] = []
   direction: { x: number; y: number } = { x: 0, y: 0 }
   cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys
   foodConsumed: boolean = false
-  directionName: string
  
   constructor() {
     super({ key: 'MainScene' })
@@ -24,6 +24,8 @@ export default class MainScene extends Phaser.Scene {
     this.fpsText = new FpsText(this)
     this.square = new Square(this, 0, 0, 0x0000ff)
     this.snakeArray.push(this.square)
+    //this.head=this.square;
+    //this.tail=this.square;
     this.food = new Square(
       this,
       Math.floor(Math.random() * (DEFAULT_WIDTH / CELL_SIZE)) * CELL_SIZE,
@@ -33,113 +35,43 @@ export default class MainScene extends Phaser.Scene {
   }
 
   update(time, delta) {
-    if (this.cursorKeys.up.isDown) {
-      if (this.directionName==='left')
-      this.snakeTurn(+40);
-      
-      if (this.directionName==='right')
-      this.snakeTurn(-40);
-      
-      this.direction = { x: 0, y: -1 }
-      this.directionName = 'up'
-    } 
-    else if (this.cursorKeys.down.isDown) {
-      if (this.directionName==='left')
-      this.snakeTurn(-40); 
-
-      if (this.directionName==='right')
-      this.snakeTurn(+40);
-
-      this.direction = { x: 0, y: 1 }
-      this.directionName = 'down'
-    } 
-    else if (this.cursorKeys.left.isDown) {
-     if (this.directionName==='up')
-     {
-     for(let i=1;i<this.snakeArray.length;i++)
-     {this.snakeArray[i].x= this.snakeArray[i-1].x+40;
-      this.snakeArray[i].y=this.snakeArray[i-1].y;
-     }
-     }
-
-     if (this.directionName==='down')
-     {
-     for(let i=1;i<this.snakeArray.length;i++)
-     {this.snakeArray[i].x= this.snakeArray[i-1].x-40;
-      this.snakeArray[i].y=this.snakeArray[i-1].y;
-     }
-     }
-      this.direction = { x: -1, y: 0 }
-      this.directionName = 'left'
-    } 
-    else if (this.cursorKeys.right.isDown) {
-      if (this.directionName==='up')
-      {
-      for(let i=1;i<this.snakeArray.length;i++)
-      {this.snakeArray[i].x= this.snakeArray[i-1].x-40;
-      this.snakeArray[i].y=this.snakeArray[i-1].y;
-      }
-      }
- 
-      if (this.directionName==='down')
-      {
-      for(let i=1;i<this.snakeArray.length;i++)
-      {this.snakeArray[i].x= this.snakeArray[i-1].x+40;
-      this.snakeArray[i].y=this.snakeArray[i-1].y;
-      }
-      }
-      this.direction = { x: 1, y: 0 }
-      this.directionName = 'right'
+    if(this.cursorKeys.up.isDown){ 
+        this.direction = {x: 0, y: -1};
+    }
+    else if(this.cursorKeys.down.isDown){ 
+        this.direction = {x: 0, y: 1}; 
+    }
+    else if(this.cursorKeys.left.isDown){  
+        this.direction = {x: -1, y: 0};
+    }
+    else if(this.cursorKeys.right.isDown){  
+        this.direction = {x: 1, y: 0};
     } 
 
     if (time >= this.square.moveTime && this.direction) {
-      this.snakeArray.forEach(square => {
-        square.move(time, this.direction)
-      })
+        this.square.move(time, this.direction,this.snakeArray)
     }
 
   this.snakeArray.forEach(square => {
     if 
     ( square.x === this.food.x && square.y=== this.food.y ) {
-    this.foodConsumed = true
+    this.foodConsumed = true;
+    //setting next random position of the food on the grid
     this.food.setPosition(
       Math.floor(Math.random() * (DEFAULT_WIDTH / CELL_SIZE)) * CELL_SIZE,
       Math.floor(Math.random() * (DEFAULT_HEIGHT / CELL_SIZE)) * CELL_SIZE
     )
-    console.log('Reached')
+    console.log('Food Consumed')
     if (this.foodConsumed) {
-      //let color=[0x0000ff,]
-      const newSquare = new Square(this, 0, 0, 0x0000ff)
-      if (this.directionName === 'down') {
-        newSquare.x = square.x
-        newSquare.y = square.y+ 40;
-      }
-      if (this.directionName === 'up') {
-        newSquare.x = square.x
-        newSquare.y = square.y - 40;
-      }
-      if (this.directionName === 'left') {
-        newSquare.x = square.x-40
-        newSquare.y = square.y ;
-      }
-      if (this.directionName === 'right') {
-        newSquare.x = square.x+40
-        newSquare.y = square.y ;
-      }
-
-      this.snakeArray.push(newSquare)
-      console.log(this.snakeArray.length)
-      this.foodConsumed = false
+      const newSquare = new Square(this, -50, -50, 0x00ff00) //newSquare created as a new part of the body
+      //added the square at (-50,-50) ie out the screen posiition because otherwise for a fraction of second 
+      /*when new square was getting created it was appearing for a fraction of second 
+      before getting added with the snakearray and repositioning in move() function 
+      this.snakeArray.push(newSquare);//inserting the square at the end of the array*/
+      this.foodConsumed=false;
     }
   }
 })
 }
 
-snakeTurn(c:number)
-{
-  for(let i=1;i<this.snakeArray.length;i++)
-  {this.snakeArray[i].y= this.snakeArray[i-1].y+c;
-   this.snakeArray[i].x=this.snakeArray[i-1].x;
-  }
-}
 }
